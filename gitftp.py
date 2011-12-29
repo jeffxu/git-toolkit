68faf2c718efc8ca4dc1f578c3046bd43c74d0e1
jeff <xtigerj@gmail.com>
2011-12-29 23:53:34 +0800

    Add logging module. Add new sys arguments to control conflict action.

--- gitftp.py
+++ gitftp.py
@@ -1,80 +1,76 @@
 #!/usr/bin/env python
-import optparse, os, sys
+import optparse, os, sys, logging
 from os import path
-from ftplib import FTP, error_perm, error_temp
-import paramiko
 
-def mkdir(path, ftp):
-    prevPath = ''
-    path = path.split('/')
-    path = path[:-1]
-    for p in path:
-        if not prevPath:
-            prevPath = p
-        else:
-            prevPath += '/' + p
-        try:
-            ftp.nlst(prevPath)
-        except error_temp:
-            ftp.mkd(prevPath)
+repoRoot = ''
 
-def cd(path, ftp):
-    if ftp.__class__ == FTP:
-        try:
-            ftp.cwd(path)
-        except error_perm:
-            print '### Could not change the remote working dir!'
-            return False
-    else:
-        try:
-            ftp.chdir(path)
-        except IOError:
-            print '### Could not change the remote working dir!'
-            return False
-    return True
+logger = logging.getLogger('git-ftp')
+formatter = logging.Formatter('%(asctime)s %(message)s')
+hdlr = logging.StreamHandler()
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
 
 def rm(remotePath, ftp, mod=1):
     ''' mod: 1 ask for futher action, remove or ignore.
              2 remove anyway.
+             3 ignore
     '''
+    global logger
+
     if mod == 1:
         reaction = raw_input('Delete the remote file: ' + remotePath + ' ? y/n')
-        if reaction == 'y':
+        if reaction.lower() == 'y':
             mod = 2
         else:
             return
-    print remotePath
 
-def upload(localPath, remotePath, ftp, conflic_mod=3):
-    ''' conflic_mod: 1 overwrite.
-                     2 ignore.
-                     3 ask user for futher action, overwrite or ignore.
+    logger.warning('Deleting... ' + remotePath)
+    ftp.rm(remotePath)
+    logger.warning('COMPLETE')
+
+def upload(localPath, remotePath, ftp, conflic_mod=1):
+    ''' conflic_mod: 1 ask user for futher action, overwrite or ignore.
+                     2 overwrite.
+                     3 ignore.
     '''
-    if ftp.__class__ == FTP:
-        try:
-            ftp.nlst(remotePath)
-            if conflic_mod == 3:
-                reaction = raw_input('Local file ' + localPath + 
-                                     ' confliced with the remote file' + remotePath + 
-                                     ' , o for overwrite, i for ignore')
-                if reaction == 'o':
-                    conflic_mod = 1
-                else:
-                    conflic_mod = 2
-        except error_temp:
-            # File not exists, continue to upload.
-            conflic_mod = 1
-        if conflic_mod == 1:
-            parent = remotePath[0:remotePath.rfind('/')]
-            try:
-                ftp.nlst(parent)
-                ftp.storbinary('STOR ' + remotePath, open(localPath, 'rb'))
-            except error_temp:
-                mkdir(remotePath, ftp)
-                ftp.storbinary('STOR ' + remotePath, open(localPath, 'rb'))
+    global logger
+
+    if ftp.isPathExists(remotePath):
+        if conflic_mod == 3:
+            reaction = raw_input('[Conflct]\nLocal: ' + localPath + '\n' + 
+                                 'Remote: ' + remotePath + '\n' + 
+                                 'w for overwrite, i for ignore: ')
+            if reaction.lower() == 'w':
+                conflic_mod = 1
+            else:
+                conflic_mod = 2
     else:
-        # sftp
-        pass
+        # File not exists, continue to upload.
+        conflic_mod = 1
+
+    if conflic_mod == 1:
+        parent = remotePath[0:remotePath.rfind('/')]
+        if not ftp.isPathExists(parent):
+            ftp.mkdir(parent)
+
+        #print localPath, remotePath
+        logger.warning('UPLOADING... ' + localPath)
+        ftp.upload(localPath, remotePath)
+        logger.warning('COMPLETE')
+    else:
+        logger.warning('IGNORE ' + localPath)
+
+def sync(localRelPath, remotePath, logs, ftp, mod=1):
+    global repoRoot
+
+    if ftp.cd(remotePath):
+        for log in logs:
+            # Filter the local path for futher target file upload or remote deletion.
+            if localRelPath == '.' or log[1].startswith(localRelPath):
+                if log[0] == 'D': # Delete remote file.
+                    rm(log[1], ftp, mod)
+                else: # upload file.
+                    upload(repoRoot + log[1], log[1], ftp, mod)
 
 def initSftpClient(config):
     transport = paramiko.Transport((config['host'], config['port']))
@@ -83,18 +79,7 @@ def initSftpClient(config):
 
     return sftp
 
-def initFtpClient(config):
-    try:
-        ftp = FTP()
-        ftp.connect(config['host'], config['port'])
-        ftp.login(config['user'], config['pwd'])
-    except error_perm:
-        print '### Login failed!'
-        return False
-
-    return ftp
-
-def readConfig(path, repoRoot):
+def readConfig(path, mode=False):
     import ConfigParser
     cfgPaser = ConfigParser.ConfigParser()
     cfgPaser.read(path)
@@ -103,18 +88,27 @@ def readConfig(path, repoRoot):
 
     # If settings not defined, wait for the user to enter host name, user name and password.
     if not cfgPaser.has_option('setting', 'host'):
-        config['host'] = raw_input('Please enter the ftp host: ')
+        if mode:
+            return False
+        else:
+            config['host'] = raw_input('Please enter the ftp host: ')
     else:
         config['host'] = cfgPaser.get('setting', 'host', 0)
 
     if not cfgPaser.has_option('setting', 'user'): 
-        config['user'] = raw_input('Please enter the ftp user name: ')
+        if mode:
+            return False
+        else:
+            config['user'] = raw_input('Please enter the ftp user name: ')
     else:
         config['user'] = cfgPaser.get('setting', 'user', 0)
 
     if not cfgPaser.has_option('setting', 'pwd'):
-        import getpass
-        config['pwd'] = getpass.getpass('Please enter the password: ')
+        if mode:
+            return False
+        else:
+            import getpass
+            config['pwd'] = getpass.getpass('Please enter the password: ')
     else:
         config['pwd'] = cfgPaser.get('setting', 'pwd', 0)
 
@@ -143,13 +137,22 @@ def readConfig(path, repoRoot):
     return config
 
 def main():
+    global repoRoot, logger
+
     from gitexport import getRepoRoot, filelog
     p = optparse.OptionParser(description="Sync files to the ftp.", 
                                 prog=os.path.basename(sys.argv[0]), 
                                 version="0.1a", 
                                 usage="%prog [WORK_DIR] -v -l [number]")
     p.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False)
-    p.add_option("--last", "-l", action="store", dest="last")
+    p.add_option("--last",    "-l", action="store", dest="last")
+    #no log messages, .gitftp.cfg must given and settings must set properly, 
+    # or app will be interrupted without messages.
+    p.add_option("--silence", "-s", action="store_false", dest="silence_mode")
+    #force overwrite and delete remote files.
+    p.add_option("--force",   "-f", action="store_false", dest="force_mode")
+    #increase only, ignore overwrite and delete.
+    p.add_option("--increase",  "-i", action="store_false", dest="increase_mode")
 
     options, arguments = p.parse_args()
     argLen = len(arguments)
@@ -162,29 +165,41 @@ def main():
     if not options.last:
         options.last = 1
 
-    repoRoot = getRepoRoot(sourceDir) + '/'
-    configPath = repoRoot + '.gitftp.cfg'
-
-    config = readConfig(configPath, repoRoot)
-    #print config
-
-    if config['ftp_type'] == 'ftp':
-        ftp = initFtpClient(config)
+    if options.silence_mode:
+        logger.setLevel(logging.CRITICAL)
+    elif options.verbose:
+        logger.setLevel(logging.INFO)
     else:
-        ftp = initSftpClient(config)
+        logger.setLevel(logging.WARNING)
 
-    if not ftp:
+    if options.force_mode:
+        mode = 2
+    elif options.increase_mode:
+        mode = 3
+    else:
+        mode = 1
+
+    repoRoot = getRepoRoot(sourceDir) + '/'
+    logger.info('Repository root: ' + repoRoot)
+
+    configPath = repoRoot + '.gitftp.cfg'
+    config = readConfig(configPath, silence_mode)
+
+    if not config:
         exit(2)
 
-    if cd(config['remote'], ftp):
-        logs = filelog(repoRoot, options.last)
-        for log in logs:
-            # Filter the local path for futher target file upload or remote deletion.
-            if config['local'] == '.' or log[1].startswith(config['local']):
-                if log[0] == 'D': # Delete remote file.
-                    rm(log[1], ftp)
-                else: # upload file.
-                    upload(repoRoot + log[1], log[1], ftp)
+    if config['ftp_type'] == 'ftp':
+        from gftpwrapper import GftpWrapper
+        ftp = GftpWrapper(config['host'], config['user'], config['pwd'], config['port'])
+    else:
+        pass
+        #ftp = initSftpClient(config)
+
+    if not ftp.loginSuccess:
+        exit(2)
+
+    logs = filelog(repoRoot, options.last)
+    sync(config['local'], config['remote'], logs, ftp, mode)
 
     ftp.close()
     # Below test file
